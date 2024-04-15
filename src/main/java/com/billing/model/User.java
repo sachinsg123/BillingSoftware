088cc9b1ae2fae56ea0a223e7864bd2f681d60f8
Mahesh Misal <maheshmisal2018@gmail.com>
2024-04-15 15:47:06 +0530

    Commit message 3.45

--- src/main/java/com/billing/model/User.java
+++ src/main/java/com/billing/model/User.java
@@ -1,7 +1,7 @@
 package com.billing.model;
 import java.util.ArrayList;
+
 import java.util.List;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
